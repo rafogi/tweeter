41b34ba87b623c1281f3e741588e2867e4b42547
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-09-09 23:48:59 +0000

    added fading error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,7 @@
-$(() => {
+
 const renderTweets = function(tweets) {
   // loops through tweets
   for(const tweet of tweets) {
-    // console.log(tweet);
     // calls createTweetElement for each tweet
     let newTweet = createTweetElement(tweet);
     // takes return value and appends it to the tweets container
@@ -17,18 +16,18 @@ const createTweetElement = function(base) {
   <header>
     <div class = tweet-head>
       <div class = 'user'>
-        <img class ='avatar' src= "${base.user.avatars}">
-        <p class ='username'>${base.user.name}</p>
+        <img class ='avatar' src= "${escape(base.user.avatars)}">
+        <p class ='username'>${escape(base.user.name)}</p>
       </div>
-      <p class = 'handle'>${base.user.handle}</p>
+      <p class = 'handle'>${escape(base.user.handle)}</p>
     </div>
   </header>
   <div class = "message">
-    <p>${base.content.text}</p>
+    <p>${escape(base.content.text)}</p>
   </div>
   <footer>
     <div class = 'tweet-foot'>
-    <p class = 'date'>${base.created_at}</p>
+    <p class = 'date'>${escape(base.created_at)}</p>
     <div class = 'ticons'>
     <i class="fas fa-flag"></i>
     <i class="fas fa-retweet"></i>
@@ -65,15 +64,26 @@ $postForm.on('submit', function (event) {
   // prevent the default browser behaviour
   event.preventDefault();
   const serializedData = $(this).serialize();
-
-  $.post('/tweets', serializedData)
-    .then(function(response) {
-    fetchPosts();
-    $(this).children('input').val('');
-
-  })
+  if (serializedData.length > 145) {
+    const $content = $('<p>').text('Too long yo!').addClass('error-message').fadeIn(200).fadeOut(4500);
+    $('.new-tweet').prepend($content);
+  } else if (serializedData.length === 5) {
+    const $content = $('<p>').text('Say something!!').addClass('error-message').fadeIn(200).fadeOut(4500);;
+    $('.new-tweet').prepend($content)
+  } else {
+    $.post('/tweets', serializedData)
+      .then(function(response) {
+      fetchPosts();
+      $(this).children('input').val('');
+    })
+  }
 })
 
-fetchPosts();
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
-})
+
+
