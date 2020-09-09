ff9e9190dac2248110f5426677a8c2830db88a96
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-09-09 21:57:06 +0000

    added create tweet element and renderTweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,73 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
+const renderTweets = function(tweets) {
+  // loops through tweets
+  for(const tweet of tweets) {
+    // console.log(tweet);
+    // calls createTweetElement for each tweet
+    let newTweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $('.old-tweets').prepend(newTweet)
+  }
+}
+
+
+const createTweetElement = function(base) {
+  const tweetHtml = `
+  <section class = tweet-container>
+  <header>
+    <div class = tweet-head>
+      <div class = 'user'>
+        <img class ='avatar' src= "${base.user.avatars}">
+        <p class ='username'>${base.user.name}</p>
+      </div>
+      <p class = 'handle'>${base.user.handle}</p>
+    </div>
+  </header>
+  <div class = "message">
+    <p>${base.content.text}</p>
+  </div>
+  <footer>
+    <div class = 'tweet-foot'>
+    <p class = 'date'>${base.created_at}</p>
+    <div class = 'ticons'>
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
+  </div>
+  </div>
+  </footer>
+</section>
+  `;
+  // $('.old-tweets').prepend(tweetHtml);
+return tweetHtml;
+}
+
+// createTweetElement(data[0]);
+
+const fetchPosts = () => {
+  // make a GET request to `/api/posts`
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    dataType: 'json',
+    success: (posts) => {
+      renderTweets(posts);
+    },
+    error: (error) => {
+      console.error(error);
+    }
+  });
+};
+
+const $postForm = $('.tweet-button');
+
+$postForm.on('submit', function (event) {
+  // prevent the default browser behaviour
+  event.preventDefault();
+  const serializedData = $(this).serialize();
+
+  $.post('/tweets', serializedData)
+  .then(function(response) {
+    console.log(response);
+  })
+})
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,4 @@
-$(document).ready( () => {
+$(document).ready( () => { 
   $('#tweetText').on("keyup", function(){
     let count = $('#tweetText').val().length;
     const max = 140
@@ -6,10 +6,11 @@ $(document).ready( () => {
     $('#counter').html(remaining);
     if (remaining < 0) {
       $('#counter').addClass('red');
-      console.log(remaining);
+      console.log(this);
     } else {
       $('#counter').removeClass('red');
     }
   })
 
 })
+                 
